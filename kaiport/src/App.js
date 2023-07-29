fbf260210ce93f4a0162707b972eea77a18f96e7
Kviponder <119442089+Kviponder@users.noreply.github.com>
2023-07-28 21:21:18 -0400

    Added structure to app

---
+++ kaiport/src/App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Nav from "./components/Nav";
+
+
+function App() {
+  return (
+    <div>
+      <Header>
+      </Header>
+      <Nav />
+
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
